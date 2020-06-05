d53d786163248e20d4ac4a27e5abbb04c8f2a99f
mfakyol <55063801+mfakyol@users.noreply.github.com>
2020-06-05 20:01:47 +0300

    sound-added-and-project-finished

--- 2-Clock/app.js
+++ 2-Clock/app.js
@@ -1,7 +1,7 @@
 const secondHand = document.querySelector('.sec-hand');
 const minsHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
-
+const tick= document.querySelector('#tick');
 
 function setDate(){
     console.log(secondHand)
@@ -12,12 +12,13 @@ function setDate(){
     const mins = now.getMinutes();
     const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) - 90;
     minsHand.style.transform = `rotate(${minsDegrees}deg)`;
-
     const hour = now.getHours();
     const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) - 90;
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+    tick.play();
 }
 
 setInterval(() => {
     setDate()
 }, 1000);
+
