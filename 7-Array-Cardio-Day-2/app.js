8dbacce4e09a69a65f0e128b4206868cd202df2e
mfakyol <55063801+mfakyol@users.noreply.github.com>
2020-07-31 01:55:16 +0300

    code-is-prettier

--- 7-Array-Cardio-Day-2/app.js
+++ 7-Array-Cardio-Day-2/app.js
@@ -48,54 +48,50 @@ commentTable.innerHTML = commentTableContent;
 
 // Some and Every Checks
 // Array.prototype.some() // is at least one person 19 or older?
-const checkAgeButton = document.querySelector('#check-age');
-const checkAgeAnswer = document.querySelector('#check-age-answer');
-checkAgeButton.addEventListener('click', () => {
-    const answer = people.some((person) => {
-        return (new Date).getFullYear() - person.year >= 19;
-    })
-    checkAgeAnswer.innerHTML = answer;
-})
-
+const checkAgeButton = document.querySelector("#check-age");
+const checkAgeAnswer = document.querySelector("#check-age-answer");
+checkAgeButton.addEventListener("click", () => {
+  const answer = people.some((person) => {
+    return new Date().getFullYear() - person.year >= 19;
+  });
+  checkAgeAnswer.innerHTML = answer;
+});
 
 // Array.prototype.every() // is everyone 19 or older?
-const checkAllAgeButton = document.querySelector('#check-all-age');
-const checkAllAgeAnswer = document.querySelector('#check-all-age-answer');
-checkAllAgeButton.addEventListener('click', () => {
-    const answer = people.every((person) => {
-        return (new Date).getFullYear() - person.year >= 19;
-    })
-    checkAllAgeAnswer.innerHTML = answer;
-})
-
-
-
+const checkAllAgeButton = document.querySelector("#check-all-age");
+const checkAllAgeAnswer = document.querySelector("#check-all-age-answer");
+checkAllAgeButton.addEventListener("click", () => {
+  const answer = people.every((person) => {
+    return new Date().getFullYear() - person.year >= 19;
+  });
+  checkAllAgeAnswer.innerHTML = answer;
+});
 
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
-const findByIdInput = document.querySelector('#find-by-id-input');
-const findByIdButton = document.querySelector('#find-by-id-button');
-const findByIdAnswer = document.querySelector('#find-by-id-answer');
+const findByIdInput = document.querySelector("#find-by-id-input");
+const findByIdButton = document.querySelector("#find-by-id-button");
+const findByIdAnswer = document.querySelector("#find-by-id-answer");
 
-findByIdButton.addEventListener('click', (e) => {
-
-    findByIdAnswer.innerHTML = comments.find((comment) => comment.id == findByIdInput.value).text
-})
-
-
+findByIdButton.addEventListener("click", (e) => {
+  findByIdAnswer.innerHTML = comments.find(
+    (comment) => comment.id == findByIdInput.value
+  ).text;
+});
 
 // Array.prototype.findIndex()
 // Find the comment with this ID
 // delete the comment with the ID of 823423
-const findIndexInput = document.querySelector('#delete-by-id-input');
-const findByIndexButton = document.querySelector('#delete-by-id-button');
+const findIndexInput = document.querySelector("#delete-by-id-input");
+const findByIndexButton = document.querySelector("#delete-by-id-button");
 
-
-findByIndexButton.addEventListener('click', (e) => {
-    const index = comments.findIndex(comment => Comment.id ==findIndexInput.value)
-    comments.splice(index,1);
-    const commentTableContent =
+findByIndexButton.addEventListener("click", (e) => {
+  const index = comments.findIndex(
+    (comment) => Comment.id == findIndexInput.value
+  );
+  comments.splice(index, 1);
+  const commentTableContent =
     "<tr><th>Id</th><th>Comment</th></tr>" +
     comments
       .map((comment) => {
@@ -108,4 +104,4 @@ findByIndexButton.addEventListener('click', (e) => {
       })
       .join("");
   commentTable.innerHTML = commentTableContent;
-})
+});
